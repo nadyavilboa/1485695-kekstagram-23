b26a86b694350e27d20983b1af6209ca8d19e594
nadya <nadya.vilboa@list.ru>
2021-06-25 01:03:51 +0300

    исправляет ошибки, добавляет модуль

--- js/input-validation.js
+++ js/input-validation.js
@@ -66,12 +66,12 @@ function printMessagesValidationHashtag() {
     inputHashtag.classList.add('input-error');
   }
 
-  else if (!resultCheckHashtags.allHashtagsCorrect) {
+  else if (!resultCheckHashtags.allHushtagsCorrect) {
     inputHashtag.setCustomValidity('Хэш-тег начинается с решётки, затем не менее 1, не более 20 символов: буквы и/или цифры');
     inputHashtag.classList.add('input-error');
   }
 
-  else if (!resultCheckHashtags.noHashtagMatches) {
+  else if (!resultCheckHashtags.noHastagMatches) {
     inputHashtag.setCustomValidity('Хэш-теги не должны повторяться. Строчные и прописные буквы не различаются');
     inputHashtag.classList.add('input-error');
   }
