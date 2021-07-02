148792a83e1a963c201ec6736cde67eeb62339fa
nadya <nadya.vilboa@list.ru>
2021-07-02 13:42:47 +0300

    Обновляет код

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 import './picture-painting.js';
-<<<<<<< HEAD
 import './popup-editor.js';
 import './input-validation.js';
-=======
 import './show-full-photo.js';
->>>>>>> 0de8fd9 (Отрисовывает полноэкранное изображение)
